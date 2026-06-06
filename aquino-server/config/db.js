5e0f92d632ca74cdedd2be0960011ed47042e4f9
Jhane Aquino <jeynloris@gmail.com>
2026-06-06 23:12:42 +0800

    fix vercel backend db connection

--- aquino-server/config/db.js
+++ aquino-server/config/db.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    // Connect MongoDB at default port 27017.
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1); // Exit process with failure
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is missing in environment variables.");
     }
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Database connection error: ${error.message}`);
+  }
 };
 
 module.exports = connectDB;
